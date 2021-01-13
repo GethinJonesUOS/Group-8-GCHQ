b00bf4597ae52df36af91198358aa81672d9c5be
Savage-chev <pl.savchev@hotmail.com>
2021-01-13 17:43:52 +0000

    initial commit ps

---
+++ index.php
@@ -0,0 +1,8 @@
+<?php
+
+$view = new stdClass();
+$view->pageTitle = 'Homepage';
+
+
+
+require_once('Views/index.phtml');
---
+++ register.php
@@ -0,0 +1,78 @@
+<?php
+
+require_once('Models/Users.php');
+
+
+$view = new stdClass();
+$view->pageTitle = 'Register';
+
+$users = new Users();
+
+//If login request submitted
+if (isset($_POST['email'], $_POST['password'])) {
+
+    //associative array = abstract data type composed of a collection of (key, value) pairs
+    $data = [
+        'email' => '',
+        'password' => '',
+        'firstname' => '',
+        'lastname' => '',
+        'confirmPassword' => '',
+        'emailError' => '',
+        'passwordError' => '',
+        'confirmPasswordError' => ''
+    ];
+
+    //Sanitize post data. Prevent sql injection
+    $_POST = filter_input_array(INPUT_POST, FILTER_SANITIZE_STRING);
+
+    //trimming the data
+    $data = [
+        'email' => trim($_POST['email']),
+        'password' => trim($_POST['password']),
+        'firstname' => trim($_POST['firstname']),
+        'lastname' => trim($_POST['lastname']),
+        'confirmPassword' => trim($_POST['confirmPassword']),
+        'emailError' => '',
+        'passwordError' => '',
+        'confirmPasswordError' => ''
+    ];
+
+
+    //call to registerValidation method in Users and pass the array
+    $view->users = $users->registerValidation($data);
+
+} else {
+    $data = [
+        'email' => '',
+        'password' => '',
+        'firstname' => '',
+        'lastname' => '',
+        'conformPassword' => '',
+        'emailError' => '',
+        'passwordError' => '',
+        'confirmPasswordError' => ''
+    ];
+}
+
+//Return $data in case of error
+if ($data['emailError']) {
+    echo $data['emailError'];
+} elseif ($data['passwordError']) {
+    echo $data['passwordError'];
+} elseif ($data['confirmPasswordError']) {
+    echo $data['confirmPasswordError'];
+} else {
+    $data = [
+        'email' => '',
+        'password' => '',
+        'firstname' => '',
+        'lastname' => '',
+        'conformPassword' => '',
+        'emailError' => '',
+        'passwordError' => '',
+        'confirmPasswordError' => ''
+    ];
+}
+
+require_once('Views/register.phtml');
